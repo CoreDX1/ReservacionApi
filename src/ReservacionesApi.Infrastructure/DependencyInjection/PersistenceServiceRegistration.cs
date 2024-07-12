dc68d8a4a4cb4c40fb49a4d59e447a38e7061b42
CoreDX1 <chrismaquis@gmail.com>
2024-07-12 12:42:07 -0300

    Mejoras

--- src/ReservacionesApi.Infrastructure/DependencyInjection/PersistenceServiceRegistration.cs
+++ src/ReservacionesApi.Infrastructure/DependencyInjection/PersistenceServiceRegistration.cs
@@ -1,15 +1,19 @@
-// namespace ReservacionesApi.Infrastructure.DependencyInjection;
-//
-// public static class PersistenceServiceRegistration
-// {
-//     public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
-//     {
-//         services.AddDbContext<ReservacionDbContext>(opttion =>
-//         {
-//             opttion.UseSqlServer(configuration.GetConnectionString("ReservacionDb"));
-//         });
-//
-//         return services;
-//     }
-// }
-//
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ReservacionesApi.Persistences.Migrations;
+
+namespace ReservacionesApi.Infrastructure.DependencyInjection;
+
+public static class PersistenceServiceRegistration
+{
+    public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ReservacionDbContext>(opttion =>
+        {
+            opttion.UseSqlServer(configuration.GetConnectionString("ReservacionDb"));
+        });
+
+        return services;
+    }
+}
