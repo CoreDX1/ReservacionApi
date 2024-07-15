21490ad8be4e9c40c3ea23c5980b0ba283e97701
CoreDX1 <chrismaquis@gmail.com>
2024-07-15 14:43:13 -0300

    Creando los servicions de User

---
+++ src/ReservacionesApi.Application/Features/Users/UserServices.cs
@@ -0,0 +1,28 @@
+using ReservacionesApi.Application.Contracts.Persistence;
+using ReservacionesApi.Application.Interfaces;
+using ReservacionesApi.Domain.Common;
+using ReservacionesApi.Domain.Entities;
+
+namespace ReservacionesApi.Application.Features.Users;
+
+public class UserService : IUserService
+{
+    private readonly IUserRepository _userRepository;
+
+    public UserService(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
+    public async Task<ApiResult<IEnumerable<User>>> UserList()
+    {
+        var users = await _userRepository.GetAllAsync();
+
+        if (!users.Any())
+        {
+            return ApiResult<IEnumerable<User>>.Error("Users not found.", 404);
+        }
+
+        return ApiResult<IEnumerable<User>>.Success(users, "Users retrieved successfully.", 200);
+    }
+}
