84daf2f0ecea928eecb89375f7498964fe3dc55a
CoreDX1 <chrismaquis@gmail.com>
2024-07-16 16:27:34 -0300

    Eliminado los parametros

--- src/ReservacionesApi.Application/Interfaces/IUserService.cs
+++ src/ReservacionesApi.Application/Interfaces/IUserService.cs
@@ -5,5 +5,5 @@ namespace ReservacionesApi.Application.Interfaces;
 
 public interface IUserService
 {
-    public Task<ApiResult<IEnumerable<User>>> UserListAsync(IReadRepository<User> repo, CancellationToken cancellationToken);
+    public Task<ApiResult<IEnumerable<User>>> UserListAsync();
 }
