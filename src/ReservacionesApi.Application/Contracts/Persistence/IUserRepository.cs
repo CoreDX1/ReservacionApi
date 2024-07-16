391aecb71369fcd41273fbe9adb05e00ed53dfe7
CoreDX1 <chrismaquis@gmail.com>
2024-07-16 16:35:47 -0300

    Creagando un comentario

--- src/ReservacionesApi.Application/Contracts/Persistence/IUserRepository.cs
+++ src/ReservacionesApi.Application/Contracts/Persistence/IUserRepository.cs
@@ -2,4 +2,5 @@ using ReservacionesApi.Domain.Entities;
 
 namespace ReservacionesApi.Application.Contracts.Persistence;
 
+// TODO: Puedes insertar nuevos metodos si es necesario.
 public interface IUserRepository : IGenericRepositoryAsync<User> { }
