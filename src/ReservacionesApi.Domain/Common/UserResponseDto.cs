6491147a93f1453521be4819b8b369f9fa5b132a
CoreDX1 <chrismaquis@gmail.com>
2024-07-17 14:16:49 -0300

    Creando la clase del DTO de User

---
+++ src/ReservacionesApi.Domain/Common/UserResponseDto.cs
@@ -0,0 +1,14 @@
+namespace ReservacionesApi.Domain.Common;
+
+public class UserResponseDto
+{
+    public int UserId { get; set; }
+
+    public string UserName { get; set; }
+
+    public string Email { get; set; }
+
+    public string FullName { get; set; }
+
+    public string PhoneNumber { get; set; }
+}
