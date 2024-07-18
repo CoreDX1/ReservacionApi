772853308ca4e467ecb73ac7bd894cdb2d8dbcce
CoreDX1 <chrismaquis@gmail.com>
2024-07-18 18:42:19 -0300

    Mejorando las consultas de la base de datos

--- src/ReservacionesApi.Persistences/Repositories/RepositoryBase.cs
+++ src/ReservacionesApi.Persistences/Repositories/RepositoryBase.cs
@@ -18,7 +18,7 @@ public class RepositoryBase<T> : IReadRepository<T>
         _configurationProvider = configurationProvider;
     }
 
-    public async Task<List<TResult>> ListAsync<TResult>()
+    public async Task<IEnumerable<TResult>> ListAsync<TResult>()
     {
         var query = await DbContext.Set<T>().ProjectTo<TResult>(_configurationProvider).ToListAsync();
         return query;
@@ -65,7 +65,7 @@ public class RepositoryBase<T> : IReadRepository<T>
         throw new NotImplementedException();
     }
 
-    public Task<List<T>> ListAsync()
+    public Task<IEnumerable<T>> ListAsync()
     {
         throw new NotImplementedException();
     }
