aa680046a32cd4e806ffaa37362e6a7bc76d3352
CoreDX1 <chrismaquis@gmail.com>
2024-07-18 20:32:24 -0300

    Tipando de codigo

--- src/ReservacionesApi.Application/Features/Users/UserServices.cs
+++ src/ReservacionesApi.Application/Features/Users/UserServices.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using ReservacionesApi.Application.Contracts.Persistence;
 using ReservacionesApi.Application.Dtos;
 using ReservacionesApi.Application.Interfaces;
@@ -17,27 +18,27 @@ public class UserService : IUserService
 
     public async Task<ApiResult<IEnumerable<UserResponseDto>>> UserListAsync()
     {
-        var users = await UnitOfWork.User.ListAsync<UserResponseDto>();
+        IEnumerable<UserResponseDto> users = await UnitOfWork.User.ListAsync<UserResponseDto>();
 
         if (!users.Any())
-            return ApiResult<IEnumerable<UserResponseDto>>.Error(ReplyMessage.Error.QueryEmpty, 404);
+            return ApiResult<IEnumerable<UserResponseDto>>.Error(ReplyMessage.Error.QueryEmpty, HttpStatusCode.NotFound);
 
-        return ApiResult<IEnumerable<UserResponseDto>>.Success(users, ReplyMessage.Success.Query, 200);
+        return ApiResult<IEnumerable<UserResponseDto>>.Success(users, ReplyMessage.Success.Query, HttpStatusCode.OK);
     }
 
     public async Task<ApiResult<UserResponseDto>> GetUserByIdAsync(int id)
     {
-        var user = await UnitOfWork.User.FindAsync<UserResponseDto>(id);
+        UserResponseDto user = await UnitOfWork.User.FindAsync<UserResponseDto>(id);
 
         if (user == null)
-            return ApiResult<UserResponseDto>.Error(ReplyMessage.Error.QueryEmpty, 404);
+            return ApiResult<UserResponseDto>.Error(ReplyMessage.Error.QueryEmpty, HttpStatusCode.NotFound);
 
-        return ApiResult<UserResponseDto>.Success(user, ReplyMessage.Success.Query, 200);
+        return ApiResult<UserResponseDto>.Success(user, ReplyMessage.Success.Query, HttpStatusCode.OK);
     }
 
     public async Task<ApiResult<int>> CountAsync()
     {
-        var count = await UnitOfWork.User.CountAsync();
-        return ApiResult<int>.Success(count, ReplyMessage.Success.Query, 200);
+        int count = await UnitOfWork.User.CountAsync();
+        return ApiResult<int>.Success(count, ReplyMessage.Success.Query, HttpStatusCode.OK);
     }
 }
