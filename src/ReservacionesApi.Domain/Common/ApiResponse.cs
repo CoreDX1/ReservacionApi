711c09ab2d42622b4691f3868dfd0501b4de7b0f
CoreDX1 <chrismaquis@gmail.com>
2024-07-18 20:32:47 -0300

    Cambiando el tipo de dato

--- src/ReservacionesApi.Domain/Common/ApiResponse.cs
+++ src/ReservacionesApi.Domain/Common/ApiResponse.cs
@@ -1,3 +1,5 @@
+using System.Net;
+
 namespace ReservacionesApi.Domain.Common;
 
 public class ApiResult<T>
@@ -6,12 +8,12 @@ public class ApiResult<T>
 
     public ResponseMetadata? Metadata { get; set; }
 
-    public static ApiResult<T> Success(T data, string message, int code)
+    public static ApiResult<T> Success(T data, string message, HttpStatusCode code)
     {
         return new ApiResult<T> { Data = data, Metadata = new ResponseMetadata(message, code) };
     }
 
-    public static ApiResult<T> Error(string message, int code)
+    public static ApiResult<T> Error(string message, HttpStatusCode code)
     {
         return new ApiResult<T> { Metadata = new ResponseMetadata(message, code) };
     }
@@ -19,11 +21,11 @@ public class ApiResult<T>
 
 public class ResponseMetadata
 {
-    public int StatusCode { get; set; }
+    public HttpStatusCode StatusCode { get; set; }
 
     public string? Message { get; set; }
 
-    public ResponseMetadata(string message, int code)
+    public ResponseMetadata(string message, HttpStatusCode code)
     {
         Message = message;
         StatusCode = code;
