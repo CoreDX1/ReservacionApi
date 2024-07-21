9fab8b1184a41bb99386a1f8ca5fd762f883673e
CoreDX1 <chrismaquis@gmail.com>
2024-07-21 19:26:08 -0300

    Agregando el mapeo de de login

--- src/ReservacionesApi.Application/Mapping/UserMapper.cs
+++ src/ReservacionesApi.Application/Mapping/UserMapper.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using ReservacionesApi.Application.Dtos;
+using ReservacionesApi.Application.Dtos.User.Request;
 using ReservacionesApi.Domain.Entities;
 
 namespace ReservacionesApi.Application.Mapping;
@@ -8,7 +9,15 @@ public class UserMapper : Profile
 {
     public UserMapper()
     {
-        CreateMap<User, UserResponseDto>();
+        // TODO: Respuesta de usuario
+        CreateMap<User, UserResponseDto>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.UserId))
+            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.UserName));
+
+        // TODO: Registrar usuario
         CreateMap<UserRequestDto, User>();
+
+        // TODO: Inicio de sesión de usuario
+        CreateMap<UserLoginRequestDto, User>();
     }
 }
