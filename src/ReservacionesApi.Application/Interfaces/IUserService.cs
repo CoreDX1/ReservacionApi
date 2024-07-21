8c82f367ed6a9e10772e609d900baef4c2128b02
CoreDX1 <chrismaquis@gmail.com>
2024-07-21 19:25:37 -0300

    Elimiando un metodo

--- src/ReservacionesApi.Application/Interfaces/IUserService.cs
+++ src/ReservacionesApi.Application/Interfaces/IUserService.cs
@@ -1,4 +1,5 @@
 using ReservacionesApi.Application.Dtos;
+using ReservacionesApi.Application.Dtos.User.Request;
 using ReservacionesApi.Domain.Common;
 using ReservacionesApi.Domain.Entities;
 
@@ -10,4 +11,5 @@ public interface IUserService
     Task<ApiResult<UserResponseDto>> GetUserByIdAsync(int id);
     Task<ApiResult<int>> CountAsync();
     Task<ApiResult<User>> AddUserAsync(UserRequestDto userRequestDto);
+    Task<ApiResult<UserResponseDto>> LoginUserAsync(UserLoginRequestDto userLoginRequestDto);
 }
