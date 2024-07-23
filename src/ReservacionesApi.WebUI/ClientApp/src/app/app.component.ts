50f571a8712be7a30f6dd918fa9aefedf92c5432
CoreDX1 <chrismaquis@gmail.com>
2024-07-23 12:56:40 -0300

    Mejorando la logica

--- src/ReservacionesApi.WebUI/ClientApp/src/app/app.component.ts
+++ src/ReservacionesApi.WebUI/ClientApp/src/app/app.component.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, NavigationStart, Router, RouterOutlet } from '@angular/router';
 import { MenuComponent } from './component/menu/menu.component';
 import { FooterComponent } from './component/footer/footer.component';
+import { NgIf } from '@angular/common';
 
 @Component({
 	selector: 'app-root',
 	standalone: true,
-	imports: [RouterOutlet, MenuComponent, FooterComponent],
+	imports: [RouterOutlet, MenuComponent, FooterComponent, NgIf],
 	templateUrl: './app.component.html',
 })
 export class AppComponent {
 	public title: string = 'Hola mundo 2222';
+
+	public showHeader: boolean = true;
+	public showFooter: boolean = true;
+
+	private readonly excludeRoutes = ['/login', '/admin'];
+
+	constructor(private router: Router) {
+		router.events.subscribe((event) => {
+			if (event instanceof NavigationStart) {
+				// TODO: Implementar spinner de carga
+				for (const route of this.excludeRoutes) {
+					if (event.url.includes(route)) {
+						this.showHeader = false;
+						this.showFooter = false;
+					}
+				}
+			}
+		});
+	}
 }
