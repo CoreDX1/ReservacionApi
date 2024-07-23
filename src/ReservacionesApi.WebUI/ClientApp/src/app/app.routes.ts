3a50dc035d41a41b745b42274742abc64cd67de0
CoreDX1 <chrismaquis@gmail.com>
2024-07-23 11:19:35 -0300

    Resto

--- src/ReservacionesApi.WebUI/ClientApp/src/app/app.routes.ts
+++ src/ReservacionesApi.WebUI/ClientApp/src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router'
+import { provideRouter, Routes } from '@angular/router';
+import LoginComponent from './component/login/login.component';
 
-export const routes: Routes = []
+export const routes: Routes = [
+	{
+		path: 'login',
+		component: LoginComponent,
+	},
+	{
+		path: '',
+		redirectTo: 'login',
+		pathMatch: 'full',
+	},
+];
+
+export const appRoutes = provideRouter(routes);
