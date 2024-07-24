38cc6b2478d6be1e449bc7a4519c7ac5513e62b0
CoreDX1 <chrismaquis@gmail.com>
2024-07-24 13:17:54 -0300

    Que la ruta sea ASP

--- src/ReservacionesApi.WebUI/ClientApp/src/app/component/menu/menu.component.ts
+++ src/ReservacionesApi.WebUI/ClientApp/src/app/component/menu/menu.component.ts
@@ -1,9 +1,33 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-menu',
 	standalone: true,
-	imports: [],
+	imports: [NgIf],
 	templateUrl: './menu.component.html',
 })
-export class MenuComponent {}
+export class MenuComponent {
+	menuOpen = false;
+
+	constructor(private router: Router) {}
+	readonly link = MenuRoutes;
+
+	navegar(path: string) {
+		this.router.navigate([path]);
+	}
+
+	toggleMunu() {
+		this.menuOpen = !this.menuOpen;
+	}
+}
+
+export enum MenuRoutes {
+	Home = '/home',
+	Login = '/login',
+	Admin = '/admin',
+	Men = '/men',
+	Women = '/women',
+	Kids = '/kids',
+}
