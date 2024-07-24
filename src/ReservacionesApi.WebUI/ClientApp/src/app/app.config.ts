a419ee315d7f5ff8a2a7548f7b000b3876bd77f8
CoreDX1 <chrismaquis@gmail.com>
2024-07-24 14:42:17 -0300

    Agregando el servicio HttpClient

--- src/ReservacionesApi.WebUI/ClientApp/src/app/app.config.ts
+++ src/ReservacionesApi.WebUI/ClientApp/src/app/app.config.ts
@@ -1,7 +1,8 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 
 import { appRoutes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-	providers: [provideZoneChangeDetection({ eventCoalescing: true }), appRoutes],
+	providers: [provideZoneChangeDetection({ eventCoalescing: true }), appRoutes, provideHttpClient()],
 };
