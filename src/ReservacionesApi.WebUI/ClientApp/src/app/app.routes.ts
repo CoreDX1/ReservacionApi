9db5e157db849dfb60a5b3eb8e1060287ecc89c1
CoreDX1 <chrismaquis@gmail.com>
2024-07-24 14:42:30 -0300

    Agregando la pagina 404

--- src/ReservacionesApi.WebUI/ClientApp/src/app/app.routes.ts
+++ src/ReservacionesApi.WebUI/ClientApp/src/app/app.routes.ts
@@ -1,19 +1,25 @@
 import { provideRouter, Routes } from '@angular/router';
-import LoginComponent from './component/login/login.component';
+import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
+import { HomeComponent } from './component/home/home.component';
+import { LoginComponent } from './component/login/login.component';
 
 export const routes: Routes = [
 	{
 		path: 'login',
-		loadComponent: () => import('./component/login/login.component'),
+		component: LoginComponent,
 	},
 	{
 		path: 'home',
-		loadComponent: () => import('./component/home/home.component'),
+		component: HomeComponent,
 	},
 	{
 		path: '',
-		redirectTo: 'login',
+		redirectTo: '/home',
 		pathMatch: 'full',
+	},
+	{
+		path: '**',
+		component: PageNotFoundComponent,
 	},
 ];
 
