4b2e68741c9f7d5f64adafd57bda4aebd681f85e
CoreDX1 <chrismaquis@gmail.com>
2024-07-29 11:05:32 -0300

    Agregando FormsModelu

--- src/ReservacionesApi.WebUI/ClientApp/src/app/app.config.ts
+++ src/ReservacionesApi.WebUI/ClientApp/src/app/app.config.ts
@@ -2,7 +2,8 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 
 import { appRoutes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 export const appConfig: ApplicationConfig = {
-	providers: [provideZoneChangeDetection({ eventCoalescing: true }), appRoutes, provideHttpClient()],
+	providers: [provideZoneChangeDetection({ eventCoalescing: true }), appRoutes, provideHttpClient(), FormsModule],
 };
