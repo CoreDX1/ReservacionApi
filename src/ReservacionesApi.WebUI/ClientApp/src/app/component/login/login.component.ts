637f1b30abda7ec94c2a0be8e117ebc06bf32479
CoreDX1 <chrismaquis@gmail.com>
2024-07-29 10:09:05 -0300

    Acreando la logica de login en el componente Login

--- src/ReservacionesApi.WebUI/ClientApp/src/app/component/login/login.component.ts
+++ src/ReservacionesApi.WebUI/ClientApp/src/app/component/login/login.component.ts
@@ -1,15 +1,34 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { UserLoginResponse, UserResponse } from '../../model/dto/user/Response/UserResponse';
 import { UserService } from '../../services/user.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { UserLoginRequest } from '../../model/dto/user/Request/UserLoginRequest';
 
 @Component({
 	selector: 'app-login',
 	standalone: true,
-	imports: [],
+	imports: [ReactiveFormsModule, FormsModule],
 	templateUrl: './login.component.html',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 	public ApiResponse = {} as UserResponse<UserLoginResponse>;
 
 	private userService = inject(UserService);
+
+	public user: UserLoginRequest = {
+		passwordHash: 'index#123',
+		email: 'chismaquis@hotmail.com',
+	};
+
+	ngOnInit(): void {
+		this.UserLogin(this.user);
+		console.log(this.ApiResponse);
+	}
+
+	public UserLogin(user: UserLoginRequest) {
+		this.userService.LoginAsync(user).subscribe((response) => {
+			this.ApiResponse = response;
+			console.log(this.ApiResponse);
+		});
+	}
 }
