0ddbfd1a63a519b20d42fce880100f37074e5b90
CoreDX1 <chrismaquis@gmail.com>
2024-07-29 11:04:21 -0300

    Nueva Ruta

---
+++ src/ReservacionesApi.Application/Dtos/User/Response/UserResponseDto.cs
@@ -0,0 +1,10 @@
+namespace ReservacionesApi.Application.Dtos.User.Response;
+
+public class UserResponseDto
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public string? FullName { get; set; }
+    public string? PhoneNumber { get; set; }
+};
